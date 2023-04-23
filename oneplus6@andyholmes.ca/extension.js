6888f26e9454b477c051832a4327d6da4769fea3
Andy Holmes <1265208+andyholmes@users.noreply.github.com>
2023-04-22 20:31:37 -0700

    feat(extension): move the clock to the left box
    Based loosely on https://gitlab.gnome.org/gnumdk/move-shell-clock

--- oneplus6@andyholmes.ca/extension.js
+++ oneplus6@andyholmes.ca/extension.js
@@ -4,6 +4,37 @@
 /* exported init */
 
 const ExtensionUtils = imports.misc.extensionUtils;
+const Main = imports.ui.main;
+
+
+/*
+ * Panel Tweaks
+ */
+function moveDateMenu(state = true) {
+    const centerBox = Main.panel?._centerBox;
+    const leftBox = Main.panel?._leftBox;
+    const dateMenu = Main.panel?.statusArea?.dateMenu;
+    
+    if (!centerBox || !leftBox || !dateMenu)
+        return;
+    
+    if (state) {
+        const centerChildren = centerBox.get_children();
+      
+        if (centerChildren.includes(dateMenu.container)) {
+            centerBox.remove_child(dateMenu.container);
+            Main.panel._leftBox.add_child(dateMenu.container);
+        }
+    } else {
+        const leftChildren = leftBox.get_children();
+
+        if (leftChildren.includes(dateMenu.container)) {
+            leftBox.remove_child(dateMenu.container);
+            centerBox.add_child(dateMenu.container);
+        }
+
+    }
+}
 
 
 class Extension {
@@ -12,9 +43,11 @@ class Extension {
     }
   
     enable() {
+        moveDateMenu(true);
     }
   
     disable() {
+        moveDateMenu(false);
     }
 }
 
